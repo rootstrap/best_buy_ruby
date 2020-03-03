09f3f94931a491ee173b284239868a7ac0e914fa
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-03-03 09:16:05 -0300

    Initial commit with ruby gem template

---
+++ lib/best_buy_ruby/base.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'best_buy_ruby/base/version'
+
+module BestBuyRuby
+  # Starting point of your gem
+  module Base
+    # Base error class
+    class Error < StandardError; end
+
+    # Your code goes here...
+  end
+end
---
+++ spec/gem/base_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe BestBuyRuby do
+  it 'has a version number' do
+    expect(BestBuyRuby::Base::VERSION).not_to be nil
+  end
+end
