b749a59cfb952403ded6f6269937a76cba63f024
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-03-06 12:45:33 -0300

    Reordered CollectionHeader attributes

--- lib/best_buy/models/collection_header.rb
+++ lib/best_buy/models/collection_header.rb
@@ -1,19 +1,19 @@
 module BestBuy
   class CollectionHeader
-    attr_reader :from, :to, :current_page, :total, :total_pages,
-                :query_time, :total_time, :partial, :canonical_url
+    attr_reader :canonical_url, :current_page, :from, :partial,
+                :query_time, :to, :total, :total_pages, :total_time
 
-    def initialize(from:, to:, current_page:, total:, total_pages:,
-                   query_time:, total_time:, partial:, canonical_url:)
-      @from = from
-      @to = to
+    def initialize(canonical_url:, current_page:, from:, partial:,
+                   query_time:, to:, total:, total_pages:, total_time:)
+      @canonical_url = canonical_url
       @current_page = current_page
+      @from = from
+      @partial = partial
+      @query_time = query_time
+      @to = to
       @total = total
       @total_pages = total_pages
-      @query_time = query_time
       @total_time = total_time
-      @partial = partial
-      @canonical_url = canonical_url
     end
   end
 end
