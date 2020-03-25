ea02af7b893371d46e76e5f74a9f07632a67a319
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-03-25 17:40:49 -0300

    Created Address model

--- lib/best_buy/models/store.rb
+++ lib/best_buy/models/store.rb
@@ -1,34 +1,26 @@
 # frozen_string_literal: true
 
-module BestBuy
-  # :reek:TooManyInstanceVariables
-  # :reek:UncommunicativeVariableName
-  class Store
-    attr_reader :address, :address2, :city, :country, :full_postal_code, :gmt_offset, :lat, :lng,
-                :location_type, :long_name, :name, :opening_time, :phone, :postal_code, :region,
-                :services, :store_id, :store_type
+require './lib/best_buy'
 
-    # rubocop:disable Metrics/AbcSize
+module BestBuy
+  class Store
+    attr_reader :location_type, :long_name, :name, :opening_time, :phone,
+                :services, :store_id, :store_type, :store_address
+
+    delegate :address, :address2, :city, :country, :full_postal_code, :gmt_offset,
+             :lat, :lng, :postal_code, :region, to: :store_address
+
     def initialize(init_params)
-      @address = init_params[:address]
-      @address2 = init_params[:address2]
-      @city = init_params[:city]
-      @country = init_params[:country]
-      @full_postal_code = init_params[:full_postal_code]
-      @gmt_offset = init_params[:gmt_offset]
-      @lat = init_params[:lat]
-      @lng = init_params[:lng]
       @location_type = init_params[:location_type]
       @long_name = init_params[:long_name]
       @name = init_params[:name]
       @opening_time = init_params[:detailed_hours]
       @phone = init_params[:phone]
-      @postal_code = init_params[:postal_code]
-      @region = init_params[:region]
       @services = init_params[:services]&.map { |service_hash| service_hash[:service] }
       @store_id = init_params[:store_id]
       @store_type = init_params[:store_type]
+
+      @store_address = Address.new(init_params)
     end
-    # rubocop:enable Metrics/AbcSize
   end
 end
